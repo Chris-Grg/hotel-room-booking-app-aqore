25fe981ae12fd061823bf3126a0aa4fbec837d5e
Chris Gurung <chris.grg7@gmail.com>
2024-03-24 09:06:26 +0545

    minor fixes related to port

--- backend/app.js
+++ backend/app.js
@@ -15,12 +15,10 @@ app.use("/api/rooms", roomsRoutes);
 app.use("/api/search", searchRoutes);
 app.use("/api/bookings", bookingsRoutes);
 
-app.listen(5000);
-
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING)
   .then(() => {
-    app.listen(7000);
+    app.listen(process.env.PORT || 7000);
   })
   .catch((err) => {
     console.log(err);
